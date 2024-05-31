198ae1d2af10f9e1b1496501950fec9c1347e0de
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-05-31 23:05:56 +0530

    using do-while loop to print 1 to 5

--- 03.js
+++ 03.js
@@ -1,16 +1,29 @@
-// 02 : Print the numbers form 1 to 5 to the console using a loop
-
+// Using a for loop
 console.log("using a for loop");
 
+// Loop from 1 to 5
 for (let i = 1; i <= 5; i++) {
-    console.log(i);
+    console.log(i); // Print the current number
 }
 
+// Using a while loop
 console.log("using a while loop");
 
-let i = 1;
+let i = 1; // Initialize i to 1
 
+// Loop while i is less than or equal to 5
 while (i <= 5) {
-    console.log(i);
-    i++;
+    console.log(i); // Print the current number
+    i++; // Increment i by 1
 }
+
+// Using a do-while loop
+console.log("using a do-while loop");
+
+let j = 1; // Initialize j to 1
+
+// Loop at least once and continue while j is less than or equal to 5
+do {
+    console.log(j); // Print the current number
+    j++; // Increment j by 1
+} while (j <= 5);
