539f213e238c97278e8d54157f7444562a7a78c2
nzaman7878 <nuruzzaman31032001@gmail.com>
2024-06-01 22:34:43 +0530

    waf to calculate area of rectangle

---
+++ 04.js
@@ -0,0 +1,11 @@
+// Create a funtion to calculate the area of a rectangle given its widht and height
+
+let calculateAreaOfRectangle = (width,height) => {
+
+    return width * height;
+
+}
+
+let area = calculateAreaOfRectangle(5,7);
+
+console.log("Area of the Rectangle is :" + area);
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 02 - Print the numbers form 1 to 5 to the console using a loop
 
 03 - Write a function to add two numbers and return the result.
+
+04 - Create a funtion to calculate the area of a rectangle given its widht and height.
